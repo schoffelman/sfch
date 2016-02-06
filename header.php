ca6718b7d16b6db24adfe22e7b28ea0adcf579e0
Schoffelman <tim.schoffelman@gmail.com>
2016-02-06 17:11:23 -0600

    updates to mobile nav & css

--- header.php
+++ header.php
@@ -24,23 +24,26 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sfch' ); ?></a>
-
+	
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-utility">
-			<ul>
-				<li class="phone"><a href="tel://6053302724" title="(605) 330-2724">(605) 330-2724</a></li>
-				<li class="contact-us"><a href="" title="Contact Us">Contact Us</a></li>
-				<li style="clear:both;"></li>
-			</ul>
+		<div class="site-header-content">
+			<div class="site-utility">
+				<ul>
+					<li class="phone"><a href="tel://6053302724" title="(605) 330-2724">(605) 330-2724</a></li>
+					<li class="contact-us"><a href="" title="Contact Us">Contact Us</a></li>
+					<li style="clear:both;"></li>
+				</ul>
+			</div>
+			<div class="site-branding">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="home"><img src="<?php bloginfo( 'template_url' ); ?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a></h1>
+			</div><!-- .site-branding -->
+			
+			<div id="overlay"></div>
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
 		</div>
-		<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="home"><img src="<?php bloginfo( 'template_url' ); ?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a></h1>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sfch' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
