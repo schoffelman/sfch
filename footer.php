b4b5b8b33e44246f90c6820de2803535d07d941f
Schoffelman <tim.schoffelman@gmail.com>
2016-02-17 20:58:05 -0600

    cleaning up footer for the desktop

--- footer.php
+++ footer.php
@@ -16,10 +16,10 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<span class="find-us">FIND US</span>
-			<span class="social"><a href="" title="LinkedIn" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-linkedin-icon.png" alt="LinkedIn" /></a><a href="" title="Facebook" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-facebook-icon.png" alt="Facebook" /></a></span>
-			<span class="location"><a href="https://goo.gl/maps/NwJpW7TUUHN2" title="Google Maps" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-location-icon.png" alt="Google Maps" /></a><a href="https://goo.gl/maps/NwJpW7TUUHN2" target="_blank" title="2000 N 4th Ave | Sioux Falls, SD 57104">2000 N 4th Ave | Sioux Falls, SD 57104</a></span>
+			<span class="social"><a href="" title="LinkedIn" target="_blank" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-linkedin-icon.png" alt="LinkedIn" /></a><a href="" title="Facebook" target="_blank" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-facebook-icon.png" alt="Facebook" /></a></span>
+			<span class="location"><a href="https://goo.gl/maps/NwJpW7TUUHN2" title="Google Maps" target="_blank" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-location-icon.png" alt="Google Maps" /></a><a href="https://goo.gl/maps/NwJpW7TUUHN2" target="_blank" title="2000 N 4th Ave | Sioux Falls, SD 57104">2000 N 4th Ave | Sioux Falls, SD 57104</a></span>
 			<span class="name">&copy; Sioux Falls Crane &amp; Hoist, Inc.</span>
-			<span class="logo"><a href="/" title="&copy; Sioux Falls Crane &amp; Hoist, Inc. logo"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-logo.jpg" alt="&amp;" /></a></span>
+			<span class="logo"><a href="/" title="&copy; Sioux Falls Crane &amp; Hoist, Inc. logo" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-logo.jpg" alt="&amp;" /></a></span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
