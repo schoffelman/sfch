dab768d2173b30516c303d12ad622cc586a92772
Schoffelman <tim.schoffelman@gmail.com>
2016-03-05 16:42:41 -0600

    linked child post listings

--- template-parts/content-child-page-list.php
+++ template-parts/content-child-page-list.php
@@ -15,28 +15,39 @@ wp_reset_query();
 // find child pages
 $child_pages = new WP_Query( array( 'post_parent' => $pageID, 'post_type' => 'page' ) );
 
-if ( $child_pages->have_posts() ) :
-	
-	while ( $child_pages->have_posts() ) : $child_pages->the_post(); ?>
-		
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		
-			<div class="child-content-listing">
-				
-				<h4><?php the_title(); ?></h4>
-				
-				<?php the_content('LEARN MORE <img src="/wp-content/themes/sfch/images/learn-more-arrow.png" alt="" />'); ?>
+if ( $child_pages->have_posts() ) : ?>
+
+<div class="child-content-listing">
+
+	<?php while ( $child_pages->have_posts() ) : $child_pages->the_post(); ?>
 			
-			</div><!-- .entry-content -->
+		<article id="post-<?php the_ID(); ?>" class="child-entry">
+				
+			<h3><?php the_title(); ?></h3>
+			
+			<?php the_content('<span>LEARN MORE</span><img src="/wp-content/themes/sfch/images/learn-more-arrow.png" alt="" />'); ?>
+			
+			<script type="text/javascript">
+			
+			if (window.jQuery) {  
+				jQuery(document).ready(function(){
+					var postID = 'post-'+<?php the_ID(); ?>;
+					jQuery('#'+postID).click(function(){
+						window.location.href = "<?php echo get_permalink(); ?>";
+					});
+				});
+			}
+			
+			</script>
 			
 		</article><!-- #post-## -->
-		
+			
 	<?php endwhile;
+		
+	wp_reset_postdata(); ?>
 	
-	wp_reset_postdata();
+</div>
 	
-endif;
-
-?>
+<?php endif; ?>
 
 
