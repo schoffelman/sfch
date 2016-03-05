843c2f744a1097ece6ac2cdac09847eb2752821e
Schoffelman <tim.schoffelman@gmail.com>
2016-03-05 14:59:47 -0600

    removed comments from pages

--- page.php
+++ page.php
@@ -41,11 +41,6 @@ while ( have_posts() ) : the_post(); ?>
 					
 				}
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
 			endwhile; // End of the loop.
 			?>
 
