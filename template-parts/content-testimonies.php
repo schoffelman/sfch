53168da61f1bbf35d532c4ab4333229ba53f464b
Schoffelman <tim.schoffelman@gmail.com>
2016-03-05 20:24:22 -0600

    updated testimony layout

--- template-parts/content-testimonies.php
+++ template-parts/content-testimonies.php
@@ -16,6 +16,49 @@
 		<?php the_content(); ?>
 		
 	</div><!-- .entry-content -->
+	
+	<?php wp_reset_query();
+		
+		// find testimonies 
+		$testimonies = new WP_Query( array(  'post_type' => 'testimonies', 'orderby' => 'menu_order', 'order' => 'ASC' ) ); 
+		
+		if ( $testimonies->have_posts() ) : 
+		
+			while ( $testimonies->have_posts() ) : $testimonies->the_post();
+			
+				echo '<div class="entry-testimonies">';
+			
+					// echo "<pre>";print_r($posts[0]);echo "</pre>";
+				
+					if ( has_post_thumbnail() ) { 
+						
+						echo '<div class="thumbnail">';
+						
+							the_post_thumbnail();
+						
+						echo '</div>';
+					
+					}  //end has_post_thumbnail()  
+					
+					echo '<div class="testimony">';
+						
+						the_content();
+						
+						the_title('<span>&#8212;','</span>'); 
+						
+					echo '</div>';
+					
+					echo '<div class="clear"></div>';
+				
+				echo '</div><!-- .entry-testimonies -->';
+			
+			endwhile;
+		
+		endif;
+		
+		wp_reset_postdata(); 
+	
+	?>
 
 	<footer class="entry-footer">
 	
