933f81bde9be169097f2892ee4160cf950cdbafb
Schoffelman <tim.schoffelman@gmail.com>
2016-03-25 17:30:30 -0500

    updated homepage footer for mobile

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<span class="find-us">FIND US</span>
 			<span class="social"><a href="" title="LinkedIn" target="_blank" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-linkedin-icon.png" alt="LinkedIn" /></a><a href="" title="Facebook" target="_blank" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-facebook-icon.png" alt="Facebook" /></a></span>
-			<span class="location"><a href="https://goo.gl/maps/NwJpW7TUUHN2" title="Google Maps" target="_blank" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-location-icon.png" alt="Google Maps" /></a><a href="https://goo.gl/maps/NwJpW7TUUHN2" target="_blank" title="2000 N 4th Ave | Sioux Falls, SD 57104">2000 N 4th Ave | Sioux Falls, SD 57104</a></span>
+			<span class="location"><a href="https://goo.gl/maps/NwJpW7TUUHN2" title="Google Maps" target="_blank" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-location-icon.png" alt="Google Maps" /></a><a href="https://goo.gl/maps/NwJpW7TUUHN2" target="_blank" title="2000 N 4th Ave | Sioux Falls, SD 57104" class="desktop">2000 N 4th Ave | Sioux Falls, SD 57104</a><a href="https://goo.gl/maps/NwJpW7TUUHN2" target="_blank" title="2000 N 4th Ave | Sioux Falls, SD 57104" class="mobile">2000 N 4th Ave<br /> Sioux Falls, SD</a></span>
 			<span class="name">&copy; Sioux Falls Crane &amp; Hoist, Inc.</span>
 			<span class="logo"><a href="/" title="&copy; Sioux Falls Crane &amp; Hoist, Inc. logo" class="img"><img src="<?php bloginfo( 'template_url' ); ?>/images/footer-logo.jpg" alt="&amp;" /></a></span>
 		</div><!-- .site-info -->
